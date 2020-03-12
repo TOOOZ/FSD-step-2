55af13b1370a370c7d3fe30b8cfc3f106d621452
TOOZ <tooz102@mail.ru>
2020-03-12 20:29:09 +0800

    Webpack settings
    Натраиваю webpack

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlagin = require('html-webpack-plugin');
+
+const PATHS = {
+    source: path.join(__dirname, 'source'),
+    build: path.join(__dirname, 'build')
+};
+
+module.exports = {
+    entry: {
+        'index': PATHS.source +'/pages/index/index.js',
+        'blog' : PATHS.source +'/pages/blog/blog.js'
+    },
+    output: {
+        path: PATHS.build,
+        filename: '[name].js'
+    },
+    plugins: [
+        new HtmlWebpackPlagin({
+            filename: 'index.html',
+            chunks: ['index'],
+            template: PATHS.source + '/pages/index/index.pug'
+        }),
+        new HtmlWebpackPlagin({
+            filename: 'blog.html',
+            chunks: ['blog'],
+            template: PATHS.source + '/pages/blog/blog.pug'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.pug$/,
+                loader: 'pug-loader',
+                options: {
+                    pretty: true
+                }
+            }
+        ]
+    }
+}
