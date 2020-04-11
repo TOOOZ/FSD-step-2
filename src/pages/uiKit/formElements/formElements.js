4af23a447a5600075a31b89623e1d07ccfcbb95b
TOOZ <tooz102@mail.ru>
2020-04-11 19:35:11 +0800

    JQuery
    Попытки подключить плагины jquery

--- src/pages/uiKit/formElements/formElements.js
+++ src/pages/uiKit/formElements/formElements.js
@@ -1,6 +1,16 @@
 import './formElements.scss';
 import './jquery.maskedinput';
 
+
 $(function(){
     $("#phone").mask("99.99.9999");
+});
+
+$(".js-range-slider").ionRangeSlider({
+    type: "double",
+    min: 0,
+    max: 1000,
+    from: 200,
+    to: 500,
+    grid: true
 });
